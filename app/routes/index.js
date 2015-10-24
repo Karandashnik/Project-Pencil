4bac79058793fe46848f7c1688431f44cd9b8524
natalieogle <natalie.ogle85@gmail.com>
2015-10-24 16:44:28 -0700

    Got passport.js working for local authentication.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,49 @@ var express = require('express');
 var router = express.Router();
 var orch = require('orchestrate');
 var config = require('../config');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var TwitterStrategy = require('passport-twitter');
+var GoogleStrategy = require('passport-google');
+var FacebookStrategy = require('passport-facebook');
 
 var db = orch(config.dbKey);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Penciled In' });
+//===============ROUTES=================
+//displays our homepage
+router.get('/', function(req, res){
+  res.render('main', {user: req.user});
 });
 
-router.post('/', function(req, res, next) {
+//displays our signup page
+router.get('/signin', function(req, res){
+  res.render('login');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+router.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+router.post('/login', passport.authenticate('local-signin', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+router.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
+
+/* router.post('/', function(req, res, next) {
 
   //Intialize a variable to store active user
   var user;
@@ -37,7 +71,9 @@ router.post('/', function(req, res, next) {
   }
   });
 
-});
+}); */
+
+
 
 
 module.exports = router;
