de24ebd911e9c8216e820439564b4620d0ebb4d1
Wendy Goodwin <wendygoodwin@mac.com>
2015-11-08 23:49:53 -0800

    clear works and edit scaffolding in place

--- app/public/javascripts/kidViews.js
+++ app/public/javascripts/kidViews.js
@@ -7,6 +7,11 @@ var UserKidsView = Backbone.View.extend({
     "click #addKid" : "createAddKidView",
   },
 
+  editKid: function() {
+    editKidView = new EditKidView({collection: main.kidCollection});
+    editKidView.render();
+    $('#kidList').append(editKidView.$el);
+  },
 
   createAddKidView: function() {
      addKidView = new AddKidView({collection: main.kidCollection});
@@ -15,26 +20,25 @@ var UserKidsView = Backbone.View.extend({
   },
 
   render: function() {
-    var addKidButton = '<button class = "addKidView btn btn-primary btn-lg" id="addKid" data-toggle="modal" data-target="#kidModal">Add Kid</button>';
+    var addKidButton = '<button class = "addKidView btn btn-warning btn-lg" id="addKid" data-toggle="modal" data-target="#kidModal">Add Child</button>';
+
+    var editChildButton = '<button class = "editKidView btn btn-success btn-lg" id="editKid" data-toggle="modal" data-target="#editModal">Edit Child</button>';
 
     var listBody = "";
     var kiddos = this.collection.pluck("kidFullName");
     console.log(kiddos);
     var title = "<h4 class='listTitle'>" + 'Registered Children' + "</h4>";
-    var editButton = "<button class = 'btn btn-warning btn-xs' 'button id='edit' 'type = button'>" + 'Edit' + "</button>";
     for (i = 0; i <kiddos.length; i++) {
     var listContents =  "<div class='row'>" +
                         "<div class='col-md-4'>" +
                         "<ul class ='listOfKids'>" +
                         "<li class='oneKid'>" + kiddos[i] + "</li>" +
-                        // "<li class='oneKid'>" + kiddos[i] + editButton + "</li>" +
-                        // "<li class='edit'>" + editButton + "</li>" +
                         "</ul>" + "</div>" +
-                        "<div class='col-md-2'>" + editButton + "</div>" +
+                        "<div class='col-md-2'>"  + "</div>" +
                         "</div>";
     listBody += listContents;
   }
-    this.$el.html(addKidButton + title + listBody);
+    this.$el.html(addKidButton + '<br>' + editChildButton + title + listBody);
   },
 
   initialize: function() {
@@ -51,7 +55,6 @@ var AddKidView = Backbone.View.extend({
   events: {
     'click #clear' : 'clear',
     'click #submit' : 'saveKid',
-    'click #cancel' : 'deleteIt',
     },
 
   clear: function(){
@@ -68,10 +71,9 @@ var AddKidView = Backbone.View.extend({
       var lastName = $('#lastName').val();
       var fullName = firstName + ' ' + midInit + ' ' + lastName;
       this.collection.create({kidFirstName: firstName, kidLastName: lastName, kidMidInitial: midInit, kidFullName: fullName, username: currentUser});
-  },
-
-  deleteIt: function() {
-    this.clear();
+      document.getElementById("firstName").value = "";
+      document.getElementById("midInit").value = "";
+      document.getElementById("lastName").value = "";
   },
 
   initialize: function() {
@@ -110,5 +112,91 @@ var AddKidView = Backbone.View.extend({
                 "</div>";
                 self.$el.html($kidModal);
     },
+});
+
+
+///////////////////////////////////////////////
+//////////////editKid View///////////////////
+///////////////////////////////////////////////
+
+var EditKidView = Backbone.View.extend({
+
+initialize: function() {
+    this.collection.on('update', this.render, this);
+},
+
+events: {
+  "click #editBox" : "editInfo",
+  "click #yesDelete" : "deleteInfo"
+},
+
+editInfo: function() {
+  // if
+},
+
+deleteInfo: function() {
+  console.log('box checked');
+
+
+},
+
+
+render: function() {
+  var self = this;
+  main.kidCollection.deferred.done(function() {
+    var kids = main.kidCollection.pluck("kidFullName");
+    var formBodyEdit = "";
+    for (i = 0; i < kids.length; i++) {
+    var contents = "<div class='row'>" +
+                       "<div class='form-group col-md-6 col-md-offset-3'>" +
+                       "<h4 class='kids'>" + kids[i] + "</h4>" +
+                       "<div id=" + kids[i] + " class='input-group'>" +
+                       "<label class='checkbox-inline' id='editBox[i]'><input type='checkbox' name=" + kids[i] + " value='Edit'>Edit</label>" +
+                       "<label class='checkbox-inline'><input type='checkbox' name=" + kids[i] + " value='Delete'>Delete </label>" + "<br>" +
+                       "<div class='row'>" +
+                       "<div class='col-md-6 col-offset-4'>" +
+                        "<input type ='text' name='firstName' id ='firstName' placeholder='Edit First Name'>" +
+                       "<input type ='text' name='midInit' id ='midInit' placeholder='Edit MI'>" +
+                       "<input type ='text' name='lastName' id ='lastName' placeholder='Edit Last Name'>" +
+                       "</div>" +
+                       "</div>" +
+                       "</div>" +
+                       "</div>" +
+                       "</div>";
+    formBodyEdit += contents;
+    }
+
+  //
+    var editModal =
+
+                "<div id='editModal' class='modal fade role='dialog'>" +
+                "<div class='modal-dialog'>" +
+                "<div class='modal-content'>" +
+                "<div class='modal-header'>" +
+                "<button type='button' class='close clear' data-dismiss='modal'>&times;</button>" +
+                "<h4 class='modal-title'>Edit Child Information</h4>" +
+                "</div>" +
+                "<div class='modal-body'>" +
+                "<div class='row'>" +
+                "<div class='col-md-4 col-md-offset-4'>" +
+                formBodyEdit +
+                "</div>" +
+                "</div>" +
+                "<button id='saveBooking' class='btn btn-primary btn-md' type='submit'>Save Changes</button>" +
+                "<button id='yesDelete' class='btn btn-danger btn-md' type='delete'>Delete Selected</button>" +
+  //               "</div>" +
+  //               "</div>" +
+  //               // "</form>" +
+  //               "</div>" +
+  //               "<div class='modal-footer'>" +
+  //               "<button type='button' class='btn btn-default clear' data-dismiss='modal'>Nevermind</button>" +
+                "</div>" +
+                "</div>" +
+                "</div>" +
+                "</div>"
+
+                self.$el.html(editModal);
+      })
+    }
 
 });
